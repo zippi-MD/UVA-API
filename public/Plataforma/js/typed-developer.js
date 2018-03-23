4b527b8105a01c84950989d0d2e1b3fe88998b39
Alejandro Mendoza <zippi180@gmail.com>
2018-03-23 14:11:24 -0600

    Updated style to show developer information

---
+++ public/Plataforma/js/typed-developer.js
@@ -0,0 +1,28 @@
+
+new Typed("#developer", {
+
+    strings: [' Made with love by Alejandro Mendoza', 'Made with love by <strong><u>Zippi</u></strong> <3'],
+
+    typeSpeed: 70,
+    startDelay: 0,
+    backSpeed: 30,
+    smartBackspace: true,
+
+    backDelay: 100,
+
+    showCursor: false,
+    cursorChar: '|',
+    autoInsertCss: false,
+
+
+    contentType: 'html'
+
+});
+
+
+
+// document.querySelector('.info').addEventListener('click', function (event) {
+//     console.log(event)
+//     var url = "http://google.com";
+//     $("<a>").attr("href", url).attr("target", "_blank")[0].click();
+// });
